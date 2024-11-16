d43c5638888fad878183fe99666450caeea3e5bc
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-11-16 11:32:01 +0000

    feat: added program config instruction

--- programs/collectivex_multisig/src/lib.rs
+++ programs/collectivex_multisig/src/lib.rs
@@ -6,11 +6,63 @@ declare_id!("8bX4XyTtZH3xGRyE1Y4tEvhvmD4GHdjiXAsEMQ39ZUBy");
 pub mod collectivex_multisig {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn program_config_init(
+        ctx: Context<ProgramConfigInit>,
+        authority: Pubkey,
+        creation_fee: u64,
+        treasury: Pubkey,
+    ) -> Result<()> {
+        // Initialize the program configuration
+        let program_config = &mut ctx.accounts.program_config;
+
+        // Set values from the arguments
+        program_config.authority = authority;
+        program_config.creation_fee = creation_fee;
+        program_config.treasury = treasury;
+
+        // Ensure validity of the configuration
+        require!(
+            program_config.authority != Pubkey::default(),
+            ErrorCode::InvalidAuthority
+        );
+        require!(
+            program_config.treasury != Pubkey::default(),
+            ErrorCode::InvalidTreasury
+        );
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct ProgramConfigInit<'info> {
+    #[account(
+        init,
+        payer = initializer,
+        space = 8 + ProgramConfig::INIT_SPACE,
+        seeds = [b"config"],
+        bump
+    )]
+    pub program_config: Account<'info, ProgramConfig>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct ProgramConfig {
+    pub authority: Pubkey, // Authority to update the config
+    pub creation_fee: u64, // Multisig creation fee
+    pub treasury: Pubkey,  // Treasury account for fees
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The authority provided is invalid.")]
+    InvalidAuthority,
+    #[msg("The treasury address is invalid.")]
+    InvalidTreasury,
+}
