891ddd8fb6dfcf5bf90eb0d9dba2b46802ff1ab2
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-11-22 00:25:58 +0000

    refactor: added transaction indexes in multisig

--- programs/collectivex_multisig/src/state/multisig.rs
+++ programs/collectivex_multisig/src/state/multisig.rs
@@ -2,6 +2,16 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
+/// Represents a multisig (multiple signature) configuration.
+///
+/// Fields:
+/// - `create_key`: The key used as a seed to generate the multisig PDA (Program Derived Address).
+/// - `config_authority`: The authority that has the permission to update the multisig configuration.
+/// - `threshold`: The number of signatures required to approve a transaction.
+/// - `members`: A vector containing the public keys of the members of the multisig. The maximum length is 10.
+/// - `time_lock`: The time lock in seconds, which specifies the delay before a transaction can be executed.
+/// - `transaction_index`: The index of the current transaction.
+/// - `stale_transaction_index`: The index of the stale transaction.
 pub struct Multisig {
     pub create_key: Pubkey,          // Key used as a seed to multisig PDA.
     pub config_authority: Pubkey,    // Authority to update the multisig
@@ -9,4 +19,6 @@ pub struct Multisig {
     #[max_len(10)]
     pub members: Vec<Pubkey>,       // Members of the multisig
     pub time_lock: u32,             // Time lock in seconds
+    pub transaction_index: u64,     //The index of the current transaction.
+    pub stale_transaction_index: u64,    // The index of the stale transaction.
 }
